cac249b0129b039091d580570753f5bf98958595
rrenwick <randy.renwick@gmail.com>
2019-06-11 23:31:42 -0400

    Initial HTML gui

---
+++ robots.js
@@ -0,0 +1,49 @@
+// Robots!
+
+
+var robots = [];
+
+
+/* inputs: 
+      numRobots: int - number of robots
+	  moveSeq: string - sequence of move characters: <, >, ^, VV
+*/
+function createSimulation(numRobots, moveSeq)
+{
+	console.log("createSimulation");	
+	console.log("numRobots: " + numRobots.toString());	
+	console.log("moveSeq: " + moveSeq);	
+	return true;
+}
+
+function executeNextStep()
+{
+	console.log("executeNextStep");	
+	return true;
+}
+
+function queryRobotPositions()
+{
+	console.log("queryRobotPositions");	
+	return "test1\ntest2\ntest3\ntest4\ntest5\n";
+}
+
+function queryHouses(filterMinPresents)
+{
+	console.log("queryNumHouses");	
+	console.log("filterMinPresents: " + filterMinPresents.toString());	
+	return 0;
+}
+
+function queryTotalPresents()
+{
+	console.log("queryTotalPresents");	
+	return 0;
+}
+
+function runSimulation()
+{
+	console.log("runSimulation");
+	return true;
+}
+
