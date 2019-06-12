bec10f2b9f94e5bc3336fdc64437a46e652b91db
rrenwick <randy.renwick@gmail.com>
2019-06-12 00:23:36 -0400

    Initial executeNextStep implementation

--- robots.js
+++ robots.js
@@ -5,6 +5,7 @@ var robots = [];
 var curStep = 0;
 var moveSeq = "";
 var world = new Map();
+var totalPresents = 0;
 
 function Coord(x, y)
 {
@@ -17,9 +18,11 @@ function WorldPos()
 	this.presents = 0;
 }
 
-function Robot(id)
+function Robot(id, x, y)
 {
 	this.id = id;
+	this.x = x;
+	this.y = y;
 }
 
 /* inputs: 
@@ -40,7 +43,10 @@ function createSimulation(numRobots, moveSeq)
 	// create the world and add origin to it
 	world = new Map();
 	world.set(Coord(0, 0), origin);
-
+	var originRow = new Map();	
+	world.set(0, originRow);
+	originRow.set(0, origin);
+	
 	// generate army of robots
 	robots = [];
 	for (var i=0; i<numRobots; i++)
@@ -48,21 +54,88 @@ function createSimulation(numRobots, moveSeq)
 		var newRobot = new Robot(i);
 		
 		// add new robot to robot list
-		robots.push(i);
+		robots.push(i, origin);
 		
 		// add new robot to origin
 		origin.robots.add(newRobot);
 	}
 	
+	totalPresents = 0;
 	curStep = 0;
 	this.moveSeq = moveSeq;
 	
 	return true;
 }
 
+function moveRobot(robot, newX, newY)
+{
+	// remove robot for old position
+	if (world.has(robot.x) && world.get(robot.x).has(robot.y))
+	{
+		var worldPos = world.get(robot.x).get(robot.y);
+		worldPos.robots.delete(robot);
+	}
+	
+	// make sure newX's row exists in the World
+	if (!world.has(newX)) world.set(newX, new Map());
+	var worldRow = world.get(newX);
+
+	// make sure newY's position exists in the row
+	if (!worldRow.has(newY)) worldRow.set(newY, new WorldPos());
+	var worldPos = worldRow.get(newY);	
+	
+	// if there is no other robots here then deliver present
+	if (worldPos.robots.size==0) 
+	{
+		worldPos.presents++;
+		totalPresents++;
+	}
+	
+	// add robot to this position
+	worldPos.robots.add(robot);
+	robot.x = newX;
+	robot.y = newY;
+}
+
 function executeNextStep()
 {
 	console.log("executeNextStep");	
+	
+	if (curStep >= moveSeq.length) return false;
+	
+	curStep++;	
+	// determine which robot to execute move on
+	var robotIndex = curStep % robots.length;
+	var robot = robots[robotIndex];
+	
+	// determine what the move is and validate it
+	var move = moveSeq[curStep];
+	if (move==='^')
+	{
+		// move up
+		moveRobot(robot, robot.x, robot.y+1);
+	}
+	else if (move==='V')
+	{
+		// move down
+		moveRobot(robot, robot.x, robot.y-1);
+	}
+	else if (move==='<')
+	{
+		// move left
+		moveRobot(robot, robot.x-1, robot.y);
+	}
+	else if (move==='>')
+	{
+		// move right
+		moveRobot(robot, robot.x+1, robot.y);
+	}
+	else
+	{
+		// invalid move
+		return false; 
+	}
+	
 	return true;
 }
 
