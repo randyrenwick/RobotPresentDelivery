508bd57ecf9296485d8e0b0ee6c5388eb3ffe89f
rrenwick <randy.renwick@gmail.com>
2019-06-11 23:58:08 -0400

    Initial createSimulation implementation

--- robots.js
+++ robots.js
@@ -2,7 +2,25 @@
 
 
 var robots = [];
+var curStep = 0;
+var moveSeq = "";
+var world = new Map();
 
+function Coord(x, y)
+{
+	return x.toString() + "," + y.toString();
+}
+
+function WorldPos()
+{
+	this.robots = new Set();
+	this.presents = 0;
+}
+
+function Robot(id)
+{
+	this.id = id;
+}
 
 /* inputs: 
       numRobots: int - number of robots
@@ -13,6 +31,32 @@ function createSimulation(numRobots, moveSeq)
 	console.log("createSimulation");	
 	console.log("numRobots: " + numRobots.toString());	
 	console.log("moveSeq: " + moveSeq);	
+
+	if (numRobots<=0) return false;
+	
+	// create the origin	
+	origin = new WorldPos();
+	
+	// create the world and add origin to it
+	world = new Map();
+	world.set(Coord(0, 0), origin);
+
+	// generate army of robots
+	robots = [];
+	for (var i=0; i<numRobots; i++)
+	{
+		var newRobot = new Robot(i);
+		
+		// add new robot to robot list
+		robots.push(i);
+		
+		// add new robot to origin
+		origin.robots.add(newRobot);
+	}
+	
+	curStep = 0;
+	this.moveSeq = moveSeq;
+	
 	return true;
 }
 
@@ -32,7 +76,7 @@ function queryHouses(filterMinPresents)
 {
 	console.log("queryNumHouses");	
 	console.log("filterMinPresents: " + filterMinPresents.toString());	
-	return 0;
+	return moveSeq;
 }
 
 function queryTotalPresents()
